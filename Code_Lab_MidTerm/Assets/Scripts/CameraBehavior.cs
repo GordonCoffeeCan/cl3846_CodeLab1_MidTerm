f09b0a8cbd4a0c3a0e05ff4b4f25e4c9c4c2dec0
Unknown <gordon.lee.6170@gmail.com>
2017-03-13 23:20:20 -0400

    character controlling script and camera following

---
+++ Code_Lab_MidTerm/Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour {
+    public float followSpeed = 5;
+
+    private Transform _cameraPivot;
+    private Transform _player;
+
+    private Vector3 _targetPos;
+
+    private void Awake() {
+        _cameraPivot = GameObject.Find("CameraPivot").transform;
+        _player = GameObject.Find("Player").transform;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _targetPos = _player.position;
+
+        _cameraPivot.position = new Vector3(Mathf.Lerp(_cameraPivot.position.x, _targetPos.x, followSpeed * Time.deltaTime), 0, Mathf.Lerp(_cameraPivot.position.z, _targetPos.z, followSpeed * Time.deltaTime));
+
+    }
+}
---
+++ Code_Lab_MidTerm/Assets/Scripts/ThirdPersonController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class ThirdPersonController : MonoBehaviour {
+    public float speed = 5;
+    public float jumpSpeed = 8;
+
+    private float _rotationSpeed = 45;
+    private float _gravity = 20;
+    private Vector3 _moveDirection = Vector3.zero;
+
+    private CharacterController _characterCtr;
+    private Transform _rotationPivot;
+
+    private void Awake() {
+        _characterCtr = this.GetComponent<CharacterController>();
+        _rotationPivot = this.transform.FindChild("RotationPivot");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (_characterCtr.isGrounded) {
+            _moveDirection = new Vector3(-Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical"));
+            _moveDirection = this.transform.TransformDirection(_moveDirection);
+            _moveDirection *= speed;
+
+            if (Input.GetButtonDown("Jump")) {
+                _moveDirection.y = jumpSpeed;
+            }
+        }
+        _moveDirection.y -= _gravity * Time.deltaTime;
+        _characterCtr.Move(_moveDirection * Time.deltaTime);
+
+        FaceToDirection(true);
+	}
+
+    private void FaceToDirection(bool _isFacingDirection) {
+        if(_isFacingDirection == true) {
+            if(_characterCtr.velocity.magnitude > 0.05f) {
+                _rotationPivot.forward += _characterCtr.velocity.normalized * _rotationSpeed * Time.deltaTime;
+                _rotationPivot.rotation = Quaternion.Euler(new Vector3(0, _rotationPivot.eulerAngles.y, 0));
+            }
+        }
+    }
+}
