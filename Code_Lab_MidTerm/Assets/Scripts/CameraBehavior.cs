358f1ca3235580cb7415a560c02bd1474787f99d
GordonCoffeeCan <gordon.lee.6170@gmail.com>
2017-03-19 20:48:40 -0400

    level design done

--- Code_Lab_MidTerm/Assets/Scripts/CameraBehavior.cs
+++ Code_Lab_MidTerm/Assets/Scripts/CameraBehavior.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraBehavior : MonoBehaviour {
     public Transform player;
     public float followSpeed = 5;
-    public float rotationSpeed = 10;
+    public float rotationSpeed = 30;
 
     private Transform _cameraPivot;
     
@@ -30,6 +30,9 @@ public class CameraBehavior : MonoBehaviour {
             _targetRot = Quaternion.Euler(new Vector3(0, player.rotation.eulerAngles.y, 0));
             _cameraPivot.position = new Vector3(Mathf.Lerp(_cameraPivot.position.x, _targetPos.x, followSpeed * Time.deltaTime), 0, Mathf.Lerp(_cameraPivot.position.z, _targetPos.z, followSpeed * Time.deltaTime));
             //_cameraPivot.rotation = Quaternion.Slerp(_cameraPivot.rotation, _targetRot, rotationSpeed * Time.deltaTime);
+
+            //Rotate Camera by Left or Right Trigger;RotateCamera
+            _cameraPivot.Rotate(new Vector3(0, Input.GetAxis("RotateCamera") * rotationSpeed * Time.deltaTime, 0));
         } else {
             Debug.LogError("Assign Transform to follow");
         }
